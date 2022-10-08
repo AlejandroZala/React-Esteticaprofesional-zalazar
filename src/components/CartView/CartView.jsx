7ad41df791040bd64dac41d30a30d062e4e6b3fe
Alejandro Zalazar <alejandrodzalazar@gmail.con>
2022-10-08 18:46:49 -0300

    agrego estilos a cartView

--- src/components/CartView/CartView.jsx
+++ src/components/CartView/CartView.jsx
@@ -1,6 +1,8 @@
 import React, {useContext} from 'react';
 import { cartCtx } from "../../context/cartContext";
-import {Link} from 'react-router-dom';
+import Button from "../Button/Button";
+import {Link} from "react-router-dom";
+import "./cartView.css";
 
 function CartView() {
   const {deleteItem} = useContext(cartCtx);
@@ -16,35 +18,41 @@ function CartView() {
   if (carroVacio === 0) {
     return (
       <div>
-        <h4>Tu carrito esta vacío</h4>
-        <Link to='/'> Ir a productos</Link>
+        <h4 className="cajaTituloCarrito">Tu carrito esta vacío</h4>
+        <Link to={"/"}>
+        <Button>Ver productos</Button>
+      </Link>
       </div>
     )
   }
     return (
-      <div> 
-        <div>
+      <section className="container">
+        <div className="cajaTituloCarrito">
           <h3>Tu carrito de compras</h3>
         </div>
-        <div>
-          {cart.map((data) => (
-            <div>
-              <h4>{data.nombre}</h4>
-              <p>Precio unitario: ${data.precio}</p>
-              <p>{data.count} unidades</p>
-              <h4>Total: ${ data.precio * data.count }</h4>
-              <button
-                onClick={() => {
-                  deleteItem(data.id);
-                  }}>Eliminar</button>
-            </div>
-          ))}
-        </div>
+        <table className="table">
+          <tbody className="cajaCarritoCompra">
+            {cart.map((data) => (
+              <tr>
+                <td>{data.nombre}</td>
+                <td>${data.precio} c/u</td>
+                <td>{data.count} unid.</td>
+                <td>Total: ${ data.precio * data.count }</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      deleteItem(data.id);
+                      }} className="btnEliminar">Eliminar</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <div>
           <h3>Total de la compra: ${ getItemPrice() }</h3>
-          <button>Finalizar mi compra</button>
+          <button className="btnComprar">Finalizar compra</button>
         </div>
-      </div>
+      </section>
     )
   }
 
