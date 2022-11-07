5f63d2ebd7b00b4b54c270f07f7cec98d6dc8b2f
Alejandro Zalazar <alejandrodzalazar@gmail.con>
2022-11-07 17:12:41 -0300

    borro comentarios

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,30 +12,7 @@ function Footer() {
     return (
         <MDBFooter bgColor="light" className="text-center text-lg-start text-muted">
         <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-            {/* <div className="me-5 d-none d-lg-block">
-            <span className="fw-bold">NUESTRAS REDES SOCIALES:</span>
-            </div> */}
 
-            {/* <div>
-            <a href="https://www.facebook.com/" className="me-4 text-reset">
-                <MDBIcon fab icon="facebook-f" />
-            </a>
-            <a href="https://www.twitter.com/" className="me-4 text-reset">
-                <MDBIcon fab icon="twitter" />
-            </a>
-            <a href="https://www.google.com/" className="me-4 text-reset">
-                <MDBIcon fab icon="google" />
-            </a>
-            <a href="https://www.instagram.com/" className="me-4 text-reset">
-                <MDBIcon fab icon="instagram" />
-            </a>
-            <a href="https://www.linkedin.com/" className="me-4 text-reset">
-                <MDBIcon fab icon="linkedin" />
-            </a>
-            <a href="https://www.github.com/" className="me-4 text-reset">
-                <MDBIcon fab icon="github" />
-            </a>
-            </div> */}
         </section>
 
         <section className="">
